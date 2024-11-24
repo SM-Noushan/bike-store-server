8ea60fe774ef7a69ecf6d708238b4fd4dd28c874
SMNoushan <noushan.md@gmail.com>
2024-11-24 23:17:49 +0600

    added timestamp to models

--- src/app/modules/order/order.model.ts
+++ src/app/modules/order/order.model.ts
@@ -1,24 +1,27 @@
 import { Schema, model } from "mongoose";
 import { TOrder } from "./order.interface";
 
-const orderSchema = new Schema<TOrder>({
-  email: {
-    type: String,
-    required: [true, "Email is required"],
+const orderSchema = new Schema<TOrder>(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+    },
+    product: {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+      required: [true, "Product Id is required"],
+    },
+    quantity: {
+      type: Number,
+      required: [true, "Bike quantity is required"],
+    },
+    totalPrice: {
+      type: Number,
+      required: [true, "Total Price is required"],
+    },
   },
-  product: {
-    type: Schema.Types.ObjectId,
-    ref: "Product",
-    required: [true, "Product Id is required"],
-  },
-  quantity: {
-    type: Number,
-    required: [true, "Bike quantity is required"],
-  },
-  totalPrice: {
-    type: Number,
-    required: [true, "Total Price is required"],
-  },
-});
+  { timestamps: true },
+);
 
 export const Order = model<TOrder>("Order", orderSchema);
