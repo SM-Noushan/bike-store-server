3e16e84802b1f6bd315ea146aec54d2fc8689eb8
SM Noushan <noushan.md@gmail.com>
2024-11-24 11:58:34 +0600

    added interface

---
+++ src/app/modules/product/product.interface.ts
@@ -0,0 +1,9 @@
+export interface TProduct {
+  name: string;
+  brand: string;
+  price: number;
+  category: "Mountain" | "Road" | "Hybrid" | "Electric";
+  description: string;
+  quantity: number;
+  inStock: boolean;
+}
